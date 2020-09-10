7cc3e9dd74e18188a08d102f8b0190d64d086248
qifei <2365515769@qq.com>
2020-09-10 11:33:29 +0800

    校验表单

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,10 +1,29 @@
 $(function() {
+    //点击去注册的链接
     $('#link_reg').on('click', function() {
-        $('.login-box').hide()
-        $('.reg-box').show()
-    })
+            $('.login-box').hide()
+            $('.reg-box').show()
+        })
+        //点击去登录的链接
     $('#link_login').on('click', function() {
-        $('.login-box').show()
-        $('.reg-box').hide()
-    })
+            $('.login-box').show()
+            $('.reg-box').hide()
+        })
+        // 从layui中获取form对象 
+    var form = layui.form
+    form.verify({
+        //我们既支持上述函数式的方式，也支持下述数组的形式
+        //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
+        pass: [
+            /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
+        ],
+        //校验两次密码是否一致
+        repwd: function(value) {
+            var pwd = $('.reg-box [name=password]').val();
+            if (pwd !== value) {
+                return '两次密码不一致';
+            }
+        }
+    });
+
 })
