a3b142a99aeef16a9a02a2d2581fe8492002ace0
qifei <2365515769@qq.com>
2020-09-10 13:51:31 +0800

    完成了注册功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -11,6 +11,7 @@ $(function() {
         })
         // 从layui中获取form对象 
     var form = layui.form
+    var layer = layui.layer;
     form.verify({
         //我们既支持上述函数式的方式，也支持下述数组的形式
         //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
@@ -25,5 +26,24 @@ $(function() {
             }
         }
     });
+
+    //监听注册表单的提交事件
+    $('#form_reg').on('submit', function(e) {
+        e.preventDefault();
+        var data = {
+            username: $('#form_reg [name=username]').val(),
+            password: $('#form_reg [name=password]').val()
+        }
+        $.post('http://ajax.frontend.itheima.net/api/reguser', data,
+            function(res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg('注册成功');
+
+                //模拟人的点击行为
+                $('#link_login').click();
+            })
+    })
 
 })
